aa98be31b59f2d168646c45b74c06bce36de8c15
Brian Kamau <bkama099@uottawa.ca>
2023-01-23 19:42:58 -0500

    added prototype home, product, and contact pages to vue.js app

--- ACandJ_Website/src/components/Home.vue
+++ ACandJ_Website/src/components/Home.vue
@@ -22,11 +22,28 @@
 
 <template>
   <div class="row">
-    <div class="centeredColumn">
-      <h1>Welcome</h1>
+    <div class="centeredColumn textWhite">
+      <h1 class="italicText titleText">Welcome</h1>
+      <h1 class="italicText jamboText">Jambo</h1>
+      <h4 class="italicText welcomeText">Crafts and Jewellery handmade in Kenya</h4>
+      <div>
+        <router-link to="/products" class="navButton">View Products</router-link>
+        <router-link to="/contact" class="navButton">Contact Us</router-link>
+      </div>
     </div>
-    <div class="centeredColumn">
-      <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="collageImg"/>
+    <div class="centeredColumn paddedColumn">
+      <div class="imagesRow">
+        <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="collageImg"/>
+        <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="collageImg"/>
+      </div>
+      <div class="imagesRow">
+        <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="collageImg"/>
+        <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="collageImg"/>
+      </div>
+      <div class="imagesRow">
+        <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="collageImg"/>
+        <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="collageImg"/>
+      </div>
     </div>
   </div>  
 </template>
@@ -40,10 +57,55 @@
     flex: 50%;
     margin-top: auto;
     margin-bottom: auto;
+    padding: 0.5rem;
     text-align: center;
   }
+  .paddedColumn {
+    padding: 1rem 0;
+  }
+  .textWhite {
+    color: white;
+  }
+  .italicText {
+    font-style: italic;
+  }
+  .titleText {
+    font-size: 10vw;
+    font-weight: 600;
+  }
+  .jamboText {
+    text-decoration: overline;
+    font-size: 2.5rem;
+    font-weight: 600;    
+  }
+  .welcomeText {
+    font-weight: lighter;
+    font-size: 1.5rem;
+    padding: 0.7rem 0;
+  }
+  .navButton {
+    display: inline-block;
+    background-color: #d49a48;
+    padding: 0.5rem 1rem;
+    margin: 1.5rem 1.5rem 0 1.5rem;
+    text-decoration: none;
+    font-size: 1rem;
+    color: white;
+    border-radius: 0.3rem;
+  }
   .collageImg {
-    width: 70%;
-    min-width: 200px;
+    width: 40%;
+    min-width: 150px;
+    margin: 0.5rem;
+    border: 0.1rem solid white;
+    border-radius: 0.2rem;
+  }
+  @media (max-width: 576px) {
+    .centeredColumn {
+      flex: auto;
+    }
+    .collageImg {
+      width: 80%;
+    }
   }
 </style>
--- ACandJ_Website/src/components/Products.vue
+++ ACandJ_Website/src/components/Products.vue
@@ -21,9 +21,42 @@
 </script>
 
 <template>
-<p>Products</p>
+  <div class="products">
+    <div class="productCard" v-for="n in 10">
+      <img src="../assets/img/IMG_1566.JPG" alt="Picture of African craft or jewellery" class="cardImg"/>
+      <h2 class="cardText">Name</h2>
+      <h2 class="cardText priceText">$Price</h2>
+    </div>
+    
+</div>
 </template>
 
 <style scoped>
-    
+  .products {
+    padding: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+  }
+  .productCard {
+    border: 0.25rem solid white;
+    border-radius: 1.5rem;
+    padding: 1rem;
+    margin: 1rem;
+  }
+  .cardImg {
+    width: 25vw;
+    min-width: 150px;
+  }
+  .cardText {
+    padding: 0.5rem;
+    font-weight: 700;
+  }
+  @media (max-width: 576px) {
+    .cardImg {
+      width: 70vw;
+    }
+  }
 </style>
